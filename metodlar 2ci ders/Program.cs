9797be1a03e7793e465a4529a746e37329d5691f
unknown <parvinvr@code.edu.az>
2022-12-13 04:24:34 +0400

    new homework

---
+++ metodlar 2ci ders/Program.cs
@@ -0,0 +1,281 @@
+﻿#region Heap-Stack
+//namespace Heap_stack
+//{
+//    internal class class1
+//    {
+//        public static void Test()
+//        {
+//            global::System.Console.WriteLine("bosh");
+//        }
+
+
+//        public static void Test(int n)
+//        {
+//            global::System.Console.WriteLine(n);
+//        }
+
+
+//        public static void Test(string n)
+//        {
+//            global::System.Console.WriteLine(n);
+//        }
+
+
+//    public static void Test(int n,string m)
+//        {
+//            global::System.Console.WriteLine(n + m);
+//        }
+
+//     }
+// }
+
+
+//int a=5
+//int b=a
+//int b=10
+//global::System.Console.WriteLine(a);
+//global::System.Console.WriteLine(b);
+
+//int[] arr1 = {1,2,3}
+//int[] arr2 = {4,5,6}
+//arr2[0] = arr1[0]
+//    global::System.Console.WriteLine(arr2[0]);
+//global::System.Console.WriteLine(arr1[0]);
+//ekranda arr2[0]=1 , arr1[0]=1 gosterecey eger arr1-i arr2-e beraber etseydik arr1[0]= 4,arr2[0]=4
+//    gostereceydi.
+
+
+
+//int n = 5;
+//Test(n)
+//    global::System.Console.WriteLine(n);
+
+//    static void Test(int n)
+//    {
+//    n = 100
+//    global::System.Console.WriteLine(n);
+//    }
+
+
+//int[] arr={ 2,4,6}
+//Test(arr[])
+//global::System.Console.WriteLine(int[]arr);
+
+
+//static void Test(arr[])
+//{
+//    arr[0]=200
+//        global::System.Console.WriteLine(arr[0]);
+//}
+
+
+
+//int n = 5;
+//Test(ref n)
+//    global::System.Console.WriteLine(n);
+
+//static void Test( ref int n)
+//{
+//    n = 100
+//    global::System.Console.WriteLine(n);
+//}
+
+
+//Eger parametrlerde ref yazsaq value type lar ozunu referance type kimi aparacaq ve n-nin 
+//    qiymeti hem moterizenin icinde hemde yuxarida deyiserek 100-e beraber olacaq.
+
+
+
+
+//int n = 5;
+//Test(out n)
+//    global::System.Console.WriteLine(n);
+
+//static void Test(out int n)
+//{
+//    n = 100
+//    global::System.Console.WriteLine(n);
+//}
+
+//string name "Pervin"
+// string name2=name
+//  name2="Cavid"
+//    global::System.Console.WriteLine(name);
+//    global::System.Console.WriteLine(name2;
+
+
+
+
+//string address="Ehmedli"
+
+//    global::System.Console.WriteLine(address.length);
+
+//    string address="Ehmedli"
+
+//    global::System.Console.WriteLine(address.startswith("e"));
+
+
+//string  address="Ehmedli"
+
+
+//   global::System.Console.WriteLine( address.endswith("i"));
+
+
+//string address="Ehmedli"
+
+//    if (address.endswith("i"))
+//	{
+//    global::System.Console.WriteLine(yes);
+//}
+//else
+//{
+//    global::System.Console.WriteLine(no);
+//}
+
+//string address="Ehmedli"
+
+//    for (int i = 0; i < address.length;i++)
+//	{
+//    global::System.Console.WriteLine(address[i]);
+//}
+
+//string address = "Ehmedli"
+
+//    if (address.contains("i")
+//	{
+//    global::System.Console.WriteLine(var);
+//}
+//else
+//{
+//    global::System.Console.WriteLine(yoxdur);
+//}
+
+
+//stiring address = "Ehmedli"
+//    var res = address.split();
+//global::System.Console.WriteLine(address)
+
+
+
+//stiring address = "Ehmedli" "Azerbaycan" "Quba"
+
+//var res=address.split(",");
+//foreach (var item in res)
+//{
+//    global::System.Console.WriteLine(item);
+//}
+
+
+//stiring address = "Ehmedli"
+
+//    global::System.Console.WriteLine(address.ToLower());
+//butun heriflerikicik yazir.
+
+//stiring address = "Ehmedli"
+
+//    global::System.Console.WriteLine(address.ToUpper());
+//butun herifleri boyuk yazir.
+
+
+
+//string name="Eflatun"
+
+//    global::System.Console.WriteLine(name.Substring(2));
+
+//Cavabi cntr+F5 eledikde "latun" cixacaq.
+
+//stiring name= "Gulendam"
+//stiring name2=" Gulendam"
+
+//     global::System.Console.WriteLine(name.Trim());
+//    global::System.Console.WriteLine(name = name2);
+
+
+
+//stiring name = "Gulendam"
+  
+//    global::System.Console.WriteLine(name.Replace("G","Q"));
+
+
+
+//string name="Behmen"
+//     if (string.IsNullOrEmpty(name))
+//	{
+//    global::System.Console.WriteLine(yes);
+//}
+//else
+//{
+//    global::System.Console.WriteLine(no);
+//}
+
+
+//string name="    "
+
+//    if (string.IsNullOrWhileSpace(name))
+//	{
+//    global::System.Console.WriteLine(yes);
+//}
+//else
+//{
+//    global::System.Console.WriteLine(no);
+
+//}
+
+
+
+//2ci tapsirigi bir nece cur yazmisam.
+
+//1ci cur
+
+
+//Getname(ref name="Cavid")
+
+//static void Getname(ref string name)
+//    string name="divaC"
+//        global::System.Console.WriteLine(name);
+
+
+
+//2ci cur
+
+//    string name="Cavid"
+
+//    global::System.Console.WriteLine(name.Replace("Cavid","divaC"));
+
+
+//3cu cur
+
+//    string name="Cavid"
+//    for (int i = 5; i <=length;i--)
+//			{
+//    if (0<i=<5)
+//    {
+//        global::System.Console.WriteLine(i);
+//    }
+//}
+//  else
+//{
+//    break;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
